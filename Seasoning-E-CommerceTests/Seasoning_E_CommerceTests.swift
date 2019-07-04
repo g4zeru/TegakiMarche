66e3fb8074be0e41905bc33366d28a202713c025
g4zeru <haru25haru02@gmail.com>
2019-07-04 12:33:14 +0900

    Initial Commit

---
+++ Seasoning-E-CommerceTests/Seasoning_E_CommerceTests.swift
@@ -0,0 +1,34 @@
+//
+//  Seasoning_E_CommerceTests.swift
+//  Seasoning-E-CommerceTests
+//
+//  Created by iniad on 2019/07/04.
+//  Copyright © 2019 g4zeru. All rights reserved.
+//
+
+import XCTest
+@testable import Seasoning_E_Commerce
+
+class Seasoning_E_CommerceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
