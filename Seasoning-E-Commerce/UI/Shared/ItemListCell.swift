b2f4453b49055df93d3b37de6837d70e624daf03
g4zeru <haru25haru02@gmail.com>
2019-08-15 00:41:36 +0900

    add shared cell

---
+++ Seasoning-E-Commerce/UI/Shared/ItemListCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemListCell.swift
+//  Seasoning-E-Commerce
+//
+//  Created by iniad on 2019/08/15.
+//
+
+import UIKit
+
+class ItemListCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
