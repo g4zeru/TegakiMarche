82b0b30ab1a4037e20f3e0ef269b14987e093167
g4zeru <haru25haru02@gmail.com>
2019-08-14 18:34:27 +0900

    add list cell

---
+++ Seasoning-E-Commerce/UI/RecommendedList/RecommendedListCell.swift
@@ -0,0 +1,17 @@
+//
+//  RecommendedListCell.swift
+//  Seasoning-E-Commerce
+//
+//  Created by iniad on 2019/08/14.
+//
+
+import UIKit
+
+class RecommendedListCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
