40e861265375fe82623f3ffe977a618d8cf942a7
g4zeru <haru25haru02@gmail.com>
2019-08-18 15:04:14 +0900

    Revert "Revert "共通で使うUIコンポーネントの実装""
    This reverts commit 3096784ac251ad66b03f0c69d8b2b0aaa4ee4684.
    
    	new file:   Seasoning-E-Commerce/Assets.xcassets/heart-icon.imageset/Contents.json
    	new file:   Seasoning-E-Commerce/Assets.xcassets/heart-icon.imageset/heart-icon.pdf
    	new file:   Seasoning-E-Commerce/Assets.xcassets/heart-square-icon.imageset/Contents.json
    	new file:   Seasoning-E-Commerce/Assets.xcassets/heart-square-icon.imageset/heart-square-icon.pdf
    	new file:   Seasoning-E-Commerce/Assets.xcassets/photo-icon.imageset/Contents.json
    	new file:   Seasoning-E-Commerce/Assets.xcassets/photo-icon.imageset/photo-icon.pdf
    	new file:   Seasoning-E-Commerce/Assets.xcassets/sharebutton-icon.imageset/Contents.json
    	new file:   Seasoning-E-Commerce/Assets.xcassets/sharebutton-icon.imageset/sharebutton-icon.pdf
    	deleted:    Seasoning-E-Commerce/Base.lproj/LaunchScreen.storyboard
    	deleted:    Seasoning-E-Commerce/Base.lproj/Main.storyboard
    	new file:   Seasoning-E-Commerce/Demo.xcassets/Contents.json
    	new file:   Seasoning-E-Commerce/Demo.xcassets/Item1.imageset/Contents.json
    	new file:   Seasoning-E-Commerce/Demo.xcassets/Item1.imageset/Item1.pdf
    	new file:   Seasoning-E-Commerce/Demo.xcassets/Item2.imageset/Contents.json
    	new file:   Seasoning-E-Commerce/Demo.xcassets/Item2.imageset/Item2.pdf
    	new file:   Seasoning-E-Commerce/Demo.xcassets/Item3.imageset/Contents.json
    	new file:   Seasoning-E-Commerce/Demo.xcassets/Item3.imageset/Item3.pdf
    	new file:   Seasoning-E-Commerce/Demo.xcassets/Item4.imageset/Contents.json
    	new file:   Seasoning-E-Commerce/Demo.xcassets/Item4.imageset/Item4.pdf
    	new file:   Seasoning-E-Commerce/Demo.xcassets/Item5.imageset/Contents.json
    	new file:   Seasoning-E-Commerce/Demo.xcassets/Item5.imageset/Item5.pdf
    	new file:   Seasoning-E-Commerce/Demo.xcassets/Item6.imageset/Contents.json
    	new file:   Seasoning-E-Commerce/Demo.xcassets/Item6.imageset/Item6.pdf
    	new file:   Seasoning-E-Commerce/Entity/Extension-Int.swift
    	new file:   Seasoning-E-Commerce/Extensions/Extension-NSAttributedString.swift
    	new file:   Seasoning-E-Commerce/Extensions/Extension-NSMutableParagraphStyle.swift
    	new file:   Seasoning-E-Commerce/Extensions/Extension-UIColor.swift
    	new file:   Seasoning-E-Commerce/Extensions/Extension-UIFont.swift
    	new file:   Seasoning-E-Commerce/Extensions/Extension-UIImage.swift
    	new file:   Seasoning-E-Commerce/UI/Shared/ItemCompactListCell.swift
    	new file:   Seasoning-E-Commerce/UI/Shared/ItemCompactListCell.xib
    	new file:   Seasoning-E-Commerce/UI/Shared/ItemListCell.swift
    	new file:   Seasoning-E-Commerce/UI/Shared/ItemListCell.xib
    	new file:   Seasoning-E-Commerce/UI/Test/TestViewController.swift
    	modified:   Seasoning-E-Commerce/ViewController.swift
    	modified:   Seasoning-E-CommerceUITests/Seasoning_E_CommerceUITests.swift

---
+++ Seasoning-E-Commerce/UI/Shared/ItemCompactListCell.swift
@@ -0,0 +1,35 @@
+//
+//  ItemCompactListCell.swift
+//  Seasoning-E-Commerce
+//
+//  Created by iniad on 2019/08/15.
+//
+
+import UIKit
+
+class ItemCompactListCell: UICollectionViewCell {
+    @IBOutlet private weak var overlayView: UIView!
+    @IBOutlet private weak var thumbnailImageView: UIImageView!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var subtitleLabel: UILabel!
+    
+    class var cellHeightRatio: CGFloat {
+        return 350/218
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.layer.masksToBounds = false
+        self.layer.shadowRadius = 3
+        self.layer.shadowOffset = CGSize(width: 2, height: 3)
+        self.layer.shadowOpacity = 0.5
+        self.overlayView.layer.masksToBounds = true
+        self.overlayView.layer.cornerRadius = 10
+    }
+    
+    func update(title: String, price: Int, image: UIImage) {
+        self.thumbnailImageView.image = image
+        self.titleLabel.text = title
+        self.subtitleLabel.text = "¥" + price.separatedByThreeDigitsWithComma
+    }
+}
--- Seasoning-E-Commerce/ViewController.swift
+++ Seasoning-E-Commerce/ViewController.swift
@@ -2,17 +2,11 @@
 //  ViewController.swift
 //  Seasoning-E-Commerce
 //
-//  Created by iniad on 2019/07/04.
-//  Copyright © 2019 g4zeru. All rights reserved.
+//  Created by iniad on 2019/08/15.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    
 }
-
