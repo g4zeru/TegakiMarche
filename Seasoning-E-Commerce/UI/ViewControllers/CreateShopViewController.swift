3dc214ec8cf91bcac0a8b03969dc5508c56ef559
g4zeru <haru25haru02@gmail.com>
2019-08-25 02:35:29 +0900

    add create shop layout

---
+++ Seasoning-E-Commerce/UI/ViewControllers/CreateShopViewController.swift
@@ -0,0 +1,20 @@
+// 
+// Copyright: © 2019, g4zeru All Rights Reserved.
+// Target: Seasoning-E-Commerce.
+// CreatedAt: 1:14.
+// GitHub: https://github.com/g4zeru/Seasoning-E-Commerce
+//
+
+import UIKit
+
+class CreateShopViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var emailSwitchView: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
