98d4466480f9fe164aa617b0ff953f50ed30916b
g4zeru <haru25haru02@gmail.com>
2019-08-29 11:33:43 +0900

    fix item

--- Seasoning-E-Commerce/Entity/Item.swift
+++ Seasoning-E-Commerce/Entity/Item.swift
@@ -6,13 +6,28 @@
 //
 
 import Ballcap
+import FirebaseStorage
 
 extension FirebaseDatastore {
-    final class Item: Object {
-        dynamic var title: String = .init()
-        dynamic var description: String = .init()
-        dynamic var price: Int = .init()
-        dynamic var rate: Double = .init()
-        dynamic var imagePath: String = .init()
+    final class Item: Modelable, Codable {
+        dynamic var title: String = ""
+        dynamic var desc: String = ""
+        ///default value
+        dynamic var price: Int = 500
+        dynamic var isPublished: Bool = true
+        dynamic var publishedAt: Date = Date()
+        dynamic var imagePath: String = ""
+    }
+}
+
+extension FirebaseDatastore.Item {
+    var storageImageRef: StorageReference {
+        return Storage.storage().reference().child(imagePath)
+    }
+}
+
+extension FirebaseDatastore.Item: FirebaseDatastoreQuery {
+    static var baseQuery: DataSource<Document<FirebaseDatastore.Item>>.Query {
+        return Document<FirebaseDatastore.Item>.query
     }
 }
