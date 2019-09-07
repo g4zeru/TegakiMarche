69f5d5949aa0b2247e923796d1e46062567c3875
g4zeru <haru25haru02@gmail.com>
2019-09-07 21:43:18 +0900

    fix Podfile.lock main.sh

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@ trap '\033[31;1mForced to exit\033[m' ERR
 
 xcodegen generate
 pod install
-cp ./IDETemplateMacros.plist ./Seasoning-E-Commerce.xcworkspace/xcshareddata/IDETemplateMacros.plist
+cp ./IDETemplateMacros.plist ./TegakiMarche.xcworkspace/xcshareddata/IDETemplateMacros.plist
 echo "-------------------------------------"
 echo -e "| \033[32;1mAll Done ❤️  \033[m Let's start Coding!! |"
 echo "-------------------------------------"
