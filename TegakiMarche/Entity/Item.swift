e2d3917095dfbe0119ed2b854f6d0ae8723ddb50
g4zeru <haru25haru02@gmail.com>
2019-09-08 16:51:20 +0900

    fix Entity

--- TegakiMarche/Entity/Item.swift
+++ TegakiMarche/Entity/Item.swift
@@ -6,9 +6,50 @@
 //
 
 import FirebaseFirestore
+import RxSwift
 
 extension Firebase {
     struct Item: FirestoreDocumentModel {
+        
+        enum ContentType: String {
+            case pickup
+            case column
+            case wallpaper
+        }
+        
+        enum AdditionalContent: FirestoreDocumentModel {
+            private enum ContentType: String {
+                case text
+                case image
+            }
+            
+            static var baseQuery: ObservableFirebaseQuery<Firebase.Item.AdditionalContent> {
+                return .init(query: Item.collection.document().collection("contents"))
+            }
+            
+            case text(String)
+            case image(URL)
+            
+            init(identity: FirestoreIdentity, json: [String : Any]) throws {
+                guard let parsedText: String = try? convert(target: parse(key: "type", json: json)) else {
+                    throw FirestoreError.parse(key: "type")
+                }
+                let contentType: ContentType? = ContentType.init(rawValue: parsedText)
+                switch contentType {
+                case .some(.image):
+                    self = .image(try convert(target: parse(key: "body", json: json)))
+                case .some(.text):
+                    self = .text(try convert(target: parse(key: "body", json: json)))
+                case .none:
+                    throw FirestoreError.unknown
+                }
+            }
+            
+            static func collectionRef(id: String) -> CollectionReference {
+                return Item.collection.document(id).collection("contents")
+            }
+        }
+        
         static var baseQuery: ObservableFirebaseQuery<Item> {
             return ObservableFirebaseQuery<Item>(query: collection)
                 .whereField("isPublished", isEqualTo: true)
@@ -16,25 +57,36 @@ extension Firebase {
         }
 
         let title: String
-        let desc: String
-        let price: Int
+        let subTitle: String?
         let isPublished: Bool
         let publishedAt: Date
-        let imagePath: String?
-        let images: [String: Any]
-
-        let identity: FirestoreIdentity
+        let thumbnailImagePath: String?
+        let createdAt: Date
+        let updatedAt: Date
+        let id: String
+        let type: ContentType
 
         init(identity: FirestoreIdentity, json: [String: Any]) throws {
-            self.identity = identity
             self.title = try convert(target: parse(key: "title", json: json))
-            self.desc = (try? convert(target: parse(key: "desc", json: json))) ?? ""
-            self.price = try convert(target: parse(key: "price", json: json))
+            self.subTitle = try? convert(target: parse(key: "desc", json: json))
             let timestamp: Timestamp = try convert(target: parse(key: "publishedAt", json: json))
             self.publishedAt = timestamp.dateValue()
             self.isPublished = try convert(target: parse(key: "isPublished", json: json))
-            self.images = [:]
-            self.imagePath = nil
+            self.thumbnailImagePath = nil
+            self.createdAt = identity.createdAt
+            self.updatedAt = identity.updatedAt
+            self.id = identity.id
+            self.type = try Item.contentType(json: json)
+        }
+        
+        static func contentType(json: [String: Any]) throws -> ContentType {
+            guard let parsedText: String = try? convert(target: parse(key: "type", json: json)) else {
+                throw FirestoreError.parse(key: "type")
+            }
+            guard let type: ContentType = ContentType.init(rawValue: parsedText) else {
+                throw FirestoreError.unknown
+            }
+            return type
         }
     }
 }
