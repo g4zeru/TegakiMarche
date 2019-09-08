a0a53f0f6f2f04f8a6030e3578fb3ad9ec5aea14
g4zeru <haru25haru02@gmail.com>
2019-09-09 00:28:31 +0900

    fix datasource

--- TegakiMarche/UI/ViewControllers/RecommendationItemsList/RecommendationItemsListViewController.swift
+++ TegakiMarche/UI/ViewControllers/RecommendationItemsList/RecommendationItemsListViewController.swift
@@ -7,6 +7,8 @@
 
 import RxSwift
 import UIKit
+import Instantiate
+import InstantiateStandard
 
 protocol RecommendationItemsListView {
     func showErrorMessage(text: String)
@@ -22,6 +24,9 @@ class RecommendationItemsListViewController: UIViewController, RecommendationIte
         view.backgroundColor = UIColor.clear
         view.contentInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
         view.register(UINib(nibName: "ListHeaderView", bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "listheader")
+        view.registerNib(type: ColumnContentCell.self)
+        view.registerNib(type: WallPaperContentCell.self)
+        view.registerNib(type: FullScreenContentCell.self)
         return view
     }()
     private let errorView = ErrorMessageView()
@@ -51,8 +56,11 @@ class RecommendationItemsListViewController: UIViewController, RecommendationIte
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
         self.store.items
-            .subscribe(onNext: { items in
+            .subscribe(onNext: { [weak self] items in
+                self?.collectionView.reloadData()
                 print(items.count)
             })
             .disposed(by: disposeBag)
@@ -74,5 +82,30 @@ class RecommendationItemsListViewController: UIViewController, RecommendationIte
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
             self.errorView.isHidden = true
         }
+    }
+}
+
+extension RecommendationItemsListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.store.items.value.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let item = self.store.items.value[indexPath.item]
+        switch item.type {
+        case .wallpaper:
+            return WallPaperContentCell.dequeue(from: collectionView, for: indexPath, with: item)
+        case .pickup:
+            return FullScreenContentCell.dequeue(from: collectionView, for: indexPath, with: item)
+        case .column:
+            return ColumnContentCell.dequeue(from: collectionView, for: indexPath, with: item)
+        }
+    }
+}
+
+extension RecommendationItemsListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width: CGFloat = collectionView.frame.width - 40
+        return CGSize(width: width, height: width * CGFloat(474.48 / 374))
     }
 }
