89a6990ffabe36201004bb3ad245cd10942b8964
g4zeru <haru25haru02@gmail.com>
2019-09-08 16:54:26 +0900

    fix cell

--- TegakiMarche/UI/ViewControllers/RecommendationItemsList/RecommendationItemsListViewController.swift
+++ TegakiMarche/UI/ViewControllers/RecommendationItemsList/RecommendationItemsListViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import RxSwift
-import SnapKit
 import UIKit
 
 protocol RecommendationItemsListView {
@@ -22,8 +21,6 @@ class RecommendationItemsListViewController: UIViewController, RecommendationIte
         let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
         view.backgroundColor = UIColor.clear
         view.contentInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
-        view.register(UINib(nibName: "RecommendedListCell", bundle: nil), forCellWithReuseIdentifier: "recommendedlist")
-        view.register(UINib(nibName: "HotRankingCell", bundle: nil), forCellWithReuseIdentifier: "hotranking")
         view.register(UINib(nibName: "ListHeaderView", bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "listheader")
         return view
     }()
@@ -95,33 +92,13 @@ extension RecommendationItemsListViewController: UICollectionViewDataSource {
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if indexPath.section == 0 {
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "recommendedlist", for: indexPath) as? RecommendedListCell else {
-                fatalError()
-            }
-            cell.update(item: store.recommendedItem[indexPath.item])
-            return cell
-        } else {
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "hotranking", for: indexPath) as? HotRankingCell else {
-                fatalError()
-            }
-            cell.update(item: store.hotrankingItem[indexPath.item])
-            return cell
-        }
+        return UICollectionViewCell()
     }
 }
 
 extension RecommendationItemsListViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let margin: CGFloat = 20
-        let contentWidth = collectionView.frame.width - margin * 2
-        if indexPath.section == 0 {
-            return CGSize(width: contentWidth, height: contentWidth * RecommendedListCell.cellHeightRatio)
-        } else {
-            let contentMargin: CGFloat = 20
-            let width = (contentWidth - contentMargin) / 2
-            return CGSize(width: width, height: width * HotRankingCell.cellHeightRatio)
-        }
+        return .zero
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
@@ -141,11 +118,7 @@ extension RecommendationItemsListViewController: UICollectionViewDelegateFlowLay
                 ) as? ListHeaderView else {
                     fatalError()
             }
-            if indexPath.section == 0 {
-                header.update(title: "Daily choice")
-            } else {
-                header.update(title: "Weekly Ranking")
-            }
+            header.update(title: "Daily choice")
             return header
         } else {
             return UICollectionReusableView()
@@ -159,10 +132,6 @@ extension RecommendationItemsListViewController: UICollectionViewDelegateFlowLay
 
 extension RecommendationItemsListViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        if indexPath.section == 0 {
-            self.store?.selectRecommendedItem(index: indexPath.item)
-        } else {
-            self.store?.selectHotRankingItem(index: indexPath.item)
-        }
+        self.store?.selectRecommendedItem(index: indexPath.item)
     }
 }
