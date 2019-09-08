4579fd6a2301bae3530bb40948d2abba23b98fbf
g4zeru <haru25haru02@gmail.com>
2019-09-09 00:28:47 +0900

    fix viewModel

--- TegakiMarche/UI/ViewControllers/RecommendationItemsList/MockReccomendationItemsListViewModel.swift
+++ TegakiMarche/UI/ViewControllers/RecommendationItemsList/MockReccomendationItemsListViewModel.swift
@@ -30,18 +30,28 @@ class MockReccomendationItemsListViewModel: RecommendationItemListInput, Recomme
     
     init() {
         let itemSubject = PublishSubject<[Firebase.Item]>()
-        let json: [String: Any] = ["title":"ttt",
-                                   "desc":"ddd",
+        let json1: [String: Any] = ["title":"夏の思い出特集！",
+                                   "desc":"夏の思い出をTegakiMarcheでも作ろう",
                                    "publishedAt": Timestamp(date: Date.create(yyyyMMdd: "20170810")!),
-                                   "imageURL":URL(string: "https://i-ogp.pximg.net/c/1050x550_80_a2_g2/img-master/img/2013/07/02/17/43/14/36786691_p0_master1200.jpg")!,
+                                   "imageURL":URL(string: "https://images.unsplash.com/photo-1520322082799-20c1288346e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80")!,
                                    "isPublished": true,
                                    "type":"pickup"]
+        let json2: [String: Any] = ["title":"Daily choice",
+                                    "desc":"iPhoneXR, iPhoneXS Max",
+                                    "publishedAt": Timestamp(date: Date.create(yyyyMMdd: "20170810")!),
+                                    "imageURL":URL(string: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=200&q=80")!,
+                                    "isPublished": true,
+                                    "type":"wallpaper"]
         reflesh
             .subscribe(onNext: { _ in
                 itemSubject.onNext([try! Firebase.Item(id: "xxx",
                                                        timestamps: Timestamps(createdAt: Date(),
                                                                               updatedAt: Date()),
-                                                       json: json)])
+                                                       json: json1),
+                                    try! Firebase.Item(id: "sss",
+                                                       timestamps: Timestamps(createdAt: Date(),
+                                                                              updatedAt: Date()),
+                                                       json: json2)])
             })
             .disposed(by: disposeBag)
         self.itemSubject = itemSubject
